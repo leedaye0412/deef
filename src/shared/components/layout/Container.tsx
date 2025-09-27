1ac95f6151dcce8285bd30e04ba56ffa8b6bb687
leedaye0412 <neoldy00@naver.com>
2025-09-27 21:11:36 +0900

    feat: 디자인 토큰 및 layout 설정

---
+++ src/shared/components/layout/Container.tsx
@@ -0,0 +1,14 @@
+type Props = React.HTMLAttributes<HTMLDivElement>;
+
+export default function Container({ className = "", ...props }: Props) {
+  return (
+    <div
+      className={[
+        "mx-auto w-full max-w-screen-2xl",
+        "px-layout-x-mobile md:px-layout-x-desktop py-layout-y",
+        className,
+      ].join(" ")}
+      {...props}
+    />
+  );
+}
