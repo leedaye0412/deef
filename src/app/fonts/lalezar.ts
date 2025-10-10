1431c1af6662efac7fd86b4c48f3c2396e4e3e70
leedaye0412 <neoldy00@naver.com>
2025-10-10 18:16:55 +0900

    feat: Lalezar 폰트 설정

---
+++ src/app/fonts/lalezar.ts
@@ -0,0 +1,9 @@
+import localFont from "next/font/local";
+
+export const lalezar = localFont({
+  src: "./Lalezar-Regular.ttf",
+  variable: "--font-lalezar",
+  weight: "400",
+  style: "normal",
+  display: "swap",
+});
