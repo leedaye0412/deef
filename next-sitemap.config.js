8a8851e1b7a68c9bee0d3cc541013d8ecb748483
leedaye0412 <neoldy00@naver.com>
2025-10-10 20:32:09 +0900

    fix: siteUrl을 "https://deef.kr"에서 https://www.deef.kr로 변경

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://deef.kr",
+  siteUrl: "https://www.deef.kr",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
